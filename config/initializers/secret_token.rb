fbf46ebc330110d289d63636734146d34f576e92
John Fink <john.fink@gmail.com>
2010-11-11 10:22:26 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barisan::Application.config.secret_token = '773654110dd8450c3b90d44e8f3405620ff22c3b0d29db314a603cef3b87080e708b24c910a9a9f6197bce48aac7c6502011631b178fe6fd36eace79b19f3433'
